feaeebf003db25338f7166bea37193036d794714
EDDxample <EDDxample@gmail.com>
2022-10-22 02:22:06 +0200

    add custom state example

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
+#![allow(dead_code)]
+
 use boilerplate::GameEngine;
-use sdl2::event::Event;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::WindowCanvas;
 
 mod boilerplate;
 
+struct CustomState {}
+
 fn main() -> Result<(), String> {
     println!("Hello, world!");
 
-    let mut game = GameEngine::new("hello world", 800, 600)
+    let mut game = GameEngine::<CustomState>::new("hello world", 800, 600)
         .set_tps(20)
         .set_fps(60)
-        .set_tick_handler(tick_handler)
         .set_render_handler(render_handler)
-        .set_keyboard_handler(keyboard_handler)
-        .set_mouse_handler(mouse_handler);
+        .set_state(CustomState {});
 
     game.run()?;
 
     Ok(())
 }
 
-fn render_handler(canvas: &mut WindowCanvas) {
+fn render_handler(canvas: &mut WindowCanvas, _state: Option<&mut CustomState>) {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     let pix_w: u32 = 800 / 255 + 1;
@@ -38,24 +39,4 @@ fn render_handler(canvas: &mut WindowCanvas) {
     }
 
     canvas.present();
-}
-
-fn tick_handler() {}
-
-fn keyboard_handler(keyboard_event: Event) {
-    match keyboard_event {
-        Event::KeyDown { .. } => (),
-        Event::KeyUp { .. } => (),
-        _ => unreachable!(),
-    }
-}
-
-fn mouse_handler(mouse_event: Event) {
-    match mouse_event {
-        Event::MouseButtonDown { .. } => (),
-        Event::MouseButtonUp { .. } => (),
-        Event::MouseMotion { .. } => (),
-        Event::MouseWheel { .. } => (),
-        _ => unreachable!(),
-    }
 }
